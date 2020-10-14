53eeb18bb59443ac661604a93ad5b987fdd79f6c
Hvarz <hvarzahar@gmail.com>
2020-10-15 00:46:15 +0300

    delete .idea

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zahar/CLionProjects/lab-03-shared_ptr
-# Build directory: /Users/zahar/CLionProjects/lab-03-shared_ptr/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/Users/zahar/CLionProjects/lab-03-shared_ptr/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/zahar/CLionProjects/lab-03-shared_ptr/CMakeLists.txt;50;add_test;/Users/zahar/CLionProjects/lab-03-shared_ptr/CMakeLists.txt;0;")
