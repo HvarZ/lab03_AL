2ee99327028e85e94d5fd6be0126191b76db9a3d
Hvarz <hvarzahar@gmail.com>
2020-10-15 13:37:54 +0300

    add last implementation

--- sources/functions.cpp
+++ sources/functions.cpp
@@ -2,12 +2,14 @@
 
 std::mutex mutex;
 
-void control_block::decrement() noexcept {
+control_block::control_block() noexcept  : counter(0){};
+
+void control_block::increment() noexcept {
   std::lock_guard<std::mutex> lg(mutex);
   ++counter;
 }
 
-void control_block::increment() noexcept {
+void control_block::decrement() noexcept {
   std::lock_guard<std::mutex> lg(mutex);
   --counter;
 }
@@ -81,3 +83,49 @@ template <typename T>
 SharedPtr<T>::operator bool() const noexcept {
   return shared_ptr != nullptr;
 }
+
+
+template <typename T>
+auto SharedPtr<T>::operator*() const -> T & {
+  return *shared_ptr;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator->() const -> T * {
+  return shared_ptr;
+}
+
+template <typename T>
+auto SharedPtr<T>::get() const -> T * {
+  return shared_ptr;
+}
+
+
+template <typename T>
+void SharedPtr<T>::reset() noexcept {
+  this->SharedPtr<T>::~SharedPtr();
+  shared_ptr = nullptr;
+  cb = nullptr;
+}
+
+template <typename T>
+void SharedPtr<T>::reset(T *ptr) {
+  this->SharedPtr<T>::~SharedPtr();
+  shared_ptr = ptr;
+  cb = new control_block;
+  cb->increment();
+}
+
+template <typename T>
+void SharedPtr<T>::swap(SharedPtr<T> &r) {
+  std::swap(shared_ptr, r.shared_ptr);
+  std::swap(cb, r.cb);
+}
+
+template <typename T>
+auto SharedPtr<T>::use_count() const -> size_t {
+  return cb->get_counter();
+}
+
+
+
